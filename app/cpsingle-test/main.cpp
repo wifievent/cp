223442869a6120bff30fd1658c824a9e4b4d4a9f
maro5397 <lsj000424@gmail.com>
2021-11-01 02:03:06 -0400

    💩 fix: fixing

--- app/cpsingle-test/main.cpp
+++ app/cpsingle-test/main.cpp
@@ -2,6 +2,10 @@
 #include <GBpFilter>
 #include <GAutoArpSpoof>
 #include <GTcpBlock>
+#include <GBlock>
+#include <GPcapDeviceWrite>
+#include <QObject>
+#include <GPacket>
 #include "myobj.h"
 
 int main(int argc, char *argv[])
@@ -20,19 +24,34 @@ int main(int argc, char *argv[])
                 Qt::DirectConnection
                 );
 
-    //GBpFilter filter;
-    //filter.filter_ = "port 80 and tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x47455420";
+    GPcapDeviceWrite writer;
+
+    GBpFilter httpfilter;
+    httpfilter.filter_ = "(port 80 and tcp[((tcp[12:1] & 0xf0) >> 2):4] = 0x47455420) && !(host 175.213.35.39)";
+
+    GBpFilter httpsfilter;
+    httpsfilter.filter_ = "tcp port 443";
 
     GTcpBlock tcpblock;
     tcpblock.backwardRst_ = false;
     tcpblock.backwardFin_ = true;
     tcpblock.backwardFinMsg_ = QStringList{"HTTP/1.0 302 Redirected\r\nLocation: http://wifievent.io\r\n\r\n"};
+    tcpblock.writer_ = &writer;
+
+    GBlock block;
 
     QObject::connect(
                 &arpspoof,
                 &GCapture::captured,
-                &tcpblock,
-                &GTcpBlock::block
+                &httpfilter,
+                &GBpFilter::check
+                );
+
+    QObject::connect(
+                &arpspoof,
+                &GCapture::captured,
+                &httpsfilter,
+                &GBpFilter::check
                 );
 
     tcpblock.open();
