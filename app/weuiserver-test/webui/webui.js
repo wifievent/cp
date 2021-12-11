51a96fb20ac1e1ad14bba893b82f77e6dcdb8635
hackloud <rladydrlaqkq@gmail.com>
2021-12-11 19:36:01 +0900

    update: login, get page list

--- app/weuiserver-test/webui/webui.js
+++ app/weuiserver-test/webui/webui.js
@@ -1,66 +1,75 @@
-let url = 'http://192.168.1.10:3001'
+let url = 'http://wifievent.io/api';
+url = 'http://localhost:3001';
 
-function buttonClick() {
-  const uid = document.getElementById('uid').value;
-  const pw = document.getElementById('pw').value;
+window.onload = async function () {
+  const sessRes = await axios.get(`${url}/user/session`, {
+    withCredentials: true
+  });
+  if (sessRes.status !== 200)
+    return;
+
+  exchangeUI();
+
+  const pageRes = await axios.get(`${url}/cp/page`, {
+    withCredentials: true,
+  }).catch(err => {
+    console.log(err.response);
+  });
+  if (pageRes.status !== 200)
+    return;
+  
+  $('#browsers').empty();
+  for (const page of pageRes.data) {
+    const option = $(`<option value='${page.id}'>${page.name}</option>`);
+    $('#browsers').append(option);
+  }
+};
+
+document.querySelector('#button').addEventListener('click', async function () {
+  const uid = document.querySelector('#uid').value;
+  const pw = document.querySelector('#pw').value;
+  if (!uid || !pw) {
+    alert('fill in the blank');
+    return;
+  }
 
   const body = { uid, pw };
 
-  let pageList = [];
-
-  axios.post(url+'/user/login', body, {
-    withCredentials: true
-  })
-  .then(function (response) {
-    if (response.status === 200) {
-      exchangeUI();
-
-      axios.get(url+"/cp/page")
-      .then(function (response) {
-        if (response.status === 200) {
-          console.log(response.data);
-          console.log(typeof(response.data));
-        }
-        //pageList.push(response.data);
-      })
-    }
-    else if(response.status === 401) {
-      window.alert("Login Fail");
-    }
-    else{
-      window.alert("error");
-    }
+  const loginRes = await axios.post(`${url}/user/login`, body, {
+    withCredentials: true,
+  }).catch(err => {
+    alert(err.response.statusText);
+    return;
   });
-}
+  if (loginRes.status !== 200)
+    return;
+
+  alert('login success');
+  window.reload();
+});
 
 function exchangeUI(pageList) {
-  $("#idbox").remove();
-  $("#pwbox").remove();
-  $("#buttonbox").remove();
-  $("#main").append(`<div class="input-wrap" id="idbox"> \
-                      <select class="input-wrap" id="browsers" name="browsers">\
-                        <option value=""></option>\
-                        <option value=""></option>\
-                        <option value=""></option>\
-                        <option value=""></option>\
-                        <option value=""></option>\
-                        <option value=""></option>\
+  $('#idbox').remove();
+  $('#pwbox').remove();
+  $('#buttonbox').remove();
+  $('#main').append(`<div class='input-wrap' id='idbox'> \
+                      <select class='input-wrap' id='browsers' name='browsers'>\
                       </select>\
                     </div> \
-                    <div class="blue-button-wrap button-wrap" id="buttonbox"> \
-                      <button type="button" onclick="cpStatus()" id="button">Start</button> \
+                    <div class='blue-button-wrap button-wrap' id='buttonbox'> \
+                      <button type='button' onclick='cpStatus()' id='button'>Start</button> \
                     </div>`);
 }
 
 function cpStatus() {
-  if($("#button").text() === "Start") {
+  if($('#button').text() === 'Start') {
     //start logic
-    $("#buttonbox").attr('class','red-button-wrap button-wrap');
-    $("#button").text("Stop");
+    $('#buttonbox').attr('class','red-button-wrap button-wrap');
+    $('#button').text('Stop');
   }
-  else if($("#button").text() === "Stop") {
+  else if($('#button').text() === 'Stop') {
     //stop logic
-    $("#buttonbox").attr('class','blue-button-wrap button-wrap');
-    $("#button").text("Start");
+    $('#buttonbox').attr('class','blue-button-wrap button-wrap');
+    $('#button').text('Start');
   }
 }
